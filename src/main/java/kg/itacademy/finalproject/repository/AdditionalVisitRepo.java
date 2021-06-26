624c6eef2276f9fb539a3a90d842e92cecefce97
elizachurokova@gmail.com <elizachurokova@gmail.com>
2021-06-27 01:08:05 +0600

    additional visit is deleted

--- src/main/java/kg/itacademy/finalproject/repository/AdditionalVisitRepo.java
+++
@@ -1,9 +0,0 @@
-package kg.itacademy.finalproject.repository;
-
-import kg.itacademy.finalproject.entity.AdditionalVisit;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AdditionalVisitRepo extends JpaRepository<AdditionalVisit, Long> {
-}
--- src/main/java/kg/itacademy/finalproject/service/AdditionalVisitService.java
+++
@@ -1,9 +0,0 @@
-package kg.itacademy.finalproject.service;
-
-import kg.itacademy.finalproject.entity.AdditionalVisit;
-import kg.itacademy.finalproject.model.AdditionalVisitModel;
-
-public interface AdditionalVisitService {
-    AdditionalVisit save(AdditionalVisitModel additionalVisitModel);
-    AdditionalVisit findById(Long id);
-}
